d44b134f2f8c69489e57a1f224385facf4c1fbf6
Eric Crosson <esc@ericcrosson.com>
2017-02-01 11:16:20 -0600

    Findup finding rc file

--- slack-notify.js
+++ slack-notify.js
@@ -2,24 +2,40 @@
 
 const _ = require('lodash');
 const util = require('util');
+const findup = require('findup');
 const Slack = require('node-slack');
 
 const spawn = require('./spawn.js');
 
+function findRcFile() {
+    const rcfilename = '.slack-notify.json';
+    let rcfile;
+    try {
+        let rcdir = findup.sync(__dirname, rcfilename);
+        rcfile = `${rcdir}/${rcfilename}`;
+    } catch(e) {
+        rcfile = false;
+    }
+    return rcfile;
+}
+
 module.exports.wrapCommand = wrapCommand;
 function wrapCommand(command) {
     return new Promise((resolve, reject) => {
+        let rcfile = findRcFile();
+        if (!rcfile) { reject('could not find rc file'); }
+        let rc = require(rcfile);
+        console.log(rc);
 
-        const cmd_array = command.split(/\s+/);
-        commandStartHook();
-        const child = spawn(cmd_array).then(code => {
-            commandEndHook(code);
+        commandStartHook(rc, command);
+        spawn(command.split(/\s+/)).then(code => {
+            commandEndHook(rc, command, code);
             resolve(code);
         });
     });
 }
 
-function commandStartHook() {
+function commandStartHook(rc, command) {
     console.log('startexechook')
     // this.slack.send({
     //     text: argv._[0],
@@ -29,7 +45,7 @@ function commandStartHook() {
     // });
 }
 
-function commandEndHook (exit_code, resolve) {
+function commandEndHook (rc, command, exit_code) {
     console.log('END hook')
     // slack.send({
     //     text: argv._[0],
