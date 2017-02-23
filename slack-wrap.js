18e989fc0ae1ced0096190b64ec1eafa709e9f66
Eric Crosson <esc@ericcrosson.com>
2017-02-23 15:35:24 -0600

    Indicate when command was interrupted
    Closes #7

--- slack-wrap.js
+++ slack-wrap.js
@@ -9,6 +9,9 @@ const date = require('date-and-time');
 
 const spawn = require('./spawn.js');
 
+var rc = {};
+var slack;
+
 function findRcFile() {
     const rcfilename = '.slack-wrap.json';
     let rcfile;
@@ -22,13 +25,16 @@ function findRcFile() {
 }
 
 module.exports.runCommand = runCommand;
+// TODO: document that arg is optional, for tests
 function runCommand(command) {
+    if (typeof command !== 'undefined') {
+        rc['command'] = command;
+    }
+
     return new Promise((resolve, reject) => {
-        spawn(command.split(/\s+/)).then(code => {
+        spawn(rc.command.split(/\s+/)).then(code => {
             resolve(code);
-        }).catch(e => {
-            reject(e);
-        });
+        }).catch(e => reject(e));
     });
 }
 
@@ -37,20 +43,40 @@ function wrapCommand(command) {
     return new Promise((resolve, reject) => {
         let rcfile = findRcFile();
         if (!rcfile) { reject('could not find rc file'); }
-        let rc = require(rcfile);
+        rc = require(rcfile);
+        rc["command"] = command;
+        slack = new Slack(rc.webhook_url);
 
-        commandStartHook(rc, command);
-        runCommand(command).then(code => {
-            commandEndHook(rc, command, code);
+        commandStartHook();
+        process.on('SIGINT', function() {
+            const msg = `Canceled: \`${command}\` on \`${os.hostname()}\``;
+            slackSync(msg).then(function() {
+                process.exit(-1);
+            }).catch(e => reject(e));
+        });
+
+        runCommand().then(code => {
+            if (code != null) {
+                commandEndHook(code);
+            }
         }).catch(e => reject(e));
     });
 }
 
-function commandStartHook(rc, command) {
-    const slack = new Slack(rc.webhook_url);
+function commandStartHook() {
     let now = new Date();
     const msg = `Action invoked on \`${os.hostname()}\` at ` +
-          `${date.format(now, 'HH:mm:ss')}: \`${command}\``;
+          `${date.format(now, 'HH:mm:ss')}: \`${rc.command}\``;
+    slackAsync(msg, rc);
+}
+
+function commandEndHook(exit_code) {
+    const msg = `\`${rc.command}\` completed on ` +
+          `\`${os.hostname()}\` with exit code \`${exit_code}\``;
+    slackAsync(msg, rc);
+}
+
+function slackAsync(msg) {
     slack.send({
         text: msg,
         channel: rc.channel,
@@ -58,13 +84,13 @@ function commandStartHook(rc, command) {
     });
 }
 
-function commandEndHook (rc, command, exit_code) {
-    const slack = new Slack(rc.webhook_url);
-    const msg = `\`${command}\` completed on ` +
-          `\`${os.hostname()}\` with exit code \`${exit_code}\``;
-    slack.send({
-        text: msg,
-        channel: rc.channel,
-        username: rc.username
+function slackSync(msg) {
+    return new Promise(function(resolve, reject) {
+        resolve(
+            slack.send({
+                text: msg,
+                channel: rc.channel,
+                username: rc.username
+            }));
     });
 }
